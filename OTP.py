409a54c261c4db32bcbb0d6cdcfa3fc693303b97
Kartik240803 <87238742+Kartik240803@users.noreply.github.com>
2021-07-29 21:12:19 +0530

    OTP project is submitted

--- OTP.py
+++ OTP.py
@@ -1,6 +1,14 @@
 import random
-otp =""
-for i in range(0,6):
-    x= random.randint(0,9)
-    otp += str(x)
+
+a = "abcdefghijklmnopqrstuvwxyz"
+b = a.upper()
+x1 = random.randint(0,9)
+x2 =random.choice(a)
+x3 =random.choice(b)
+c = [x1,x2,x3]
+otp=""
+print(x3)
+for i in range(6):
+    otp += str(random.choice(c))
+
 print(f"You One Time password is : {otp}")
